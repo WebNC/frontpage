7d8c49330e51d82bbd6e6f2803a151588fe0799a
Phan Thi Mai <ptmaimai106@gmail.com>
2019-12-15 17:16:20 +0700

    Edit Footer

--- src/components/Introduction/Intro.js
+++ src/components/Introduction/Intro.js
@@ -3,14 +3,17 @@ import './Intro.scss';
 
 const feature =  [{
         name: 'Tìm kiếm giáo viên',
+        src: './logo.png',
         description : 'Xem danh sách các giáo viên cũng như các thông tin chi tiết của giáo viên để tìm được giáo viên phù hợp với mình nhất '
     },
     {
       name: 'Đăng tải thông tin giáo viên',
+      src:'./teacher.png',
       description : 'Đăng tải thông tin để các học viên có thể tìm đến mình một cách nhanh nhất'
   },
   {
     name: 'Uy tín và chất lượng cao ',
+    src: './uytin.png',
     description : 'Phản hồi của các học viên về website cũng như giáo viên cho thấy độ tin cậy rất cao'
 }
 ]
@@ -25,7 +28,7 @@ class Intro extends React.Component {
       <div className="features d-flex justify-content-between">
           {
               feature.map((item, index) => <div className="feature" key={index}>
-                  <img src="http://placehold.it/1000" height="80" width="80" alt="img" />
+                  <img src={item.src} height="100" width="100" alt="img" />
                  <h4>{item.name}</h4>
                  <p>{item.description}</p>
               </div>)
