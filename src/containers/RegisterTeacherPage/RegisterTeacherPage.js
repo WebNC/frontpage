6e125f8c4f1bc4611dadb837293aee5b464ffbc2
duyminh256 <1612384@student.hcmus.edu.vn>
2019-12-18 10:30:19 +0700

    update validate

--- src/containers/RegisterTeacherPage/RegisterTeacherPage.js
+++ src/containers/RegisterTeacherPage/RegisterTeacherPage.js
@@ -56,7 +56,7 @@ class RegistrationTeacherForm extends React.Component {
     }
     checkPhoneNumber = (rule, value, callback) => {
       const isVNPhoneMobile = /^(0|\+84)(\s|\.)?((3[2-9])|(5[689])|(7[06-9])|(8[1-689])|(9[0-46-9]))(\d)(\s|\.)?(\d{3})(\s|\.)?(\d{3})$/;
-      if (isVNPhoneMobile(value) === false) {
+      if (isVNPhoneMobile.test(value) === false) {
         callback('Số điện thoại không hợp lệ');
       } else {
         callback();
